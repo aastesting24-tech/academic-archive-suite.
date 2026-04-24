1bb430b3150f3bd92c47e7dd7fa0c9e7af3ca642
aastesting24-tech <aastesting24@gmail.com>
2026-04-24 22:33:06 +0530

    Refactor courseData structure and user sign-in logic

--- script.js
+++ script.js
@@ -71,7 +71,6 @@ function handleCredentialResponse(response) {
 	const SHEET_ID = "1i8w-_jRwhOmjNxflLmqN80m8tPwDFUcJ2dON9yyywkQ";   // e.g. 1i8w-_jRwhOmjNxflLmqN80m8tPwDFUcJ2dON9yyywkQ
     const SHEET_NAME = "AccessApproval";        // must match tab name exactly
     const URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json&sheet=${SHEET_NAME}`;
-	const email = responsePayload.email;
 	checkEmail();
 	
     async function checkEmail() {
